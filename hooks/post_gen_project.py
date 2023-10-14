014b4fc5927cac6311cf263d8d06d1bd46b9820b
alec42 <alecvanrassel@gmail.com>
2023-10-14 16:05:32 -0400

    Cleanup
    - Remove unnecessary (as of now) files & folders
    - Remove code from cookiecutter.json relating to these removed files
    - Remove code from Makefile relating to these removed files
    - Remove code from pre_gen hooks relating to these removed files
    - Remove code from post_gen hooks relating to these removed files
    - Add environment.yml file template
    - Cleanup readme

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -6,37 +6,6 @@ import re
 # Variables from cookiecutter
 repo_name = '{{ cookiecutter.__repo_name }}'
 
-# https://github.com/cookiecutter/cookiecutter/issues/824
-#   our workaround is to include these utility functions in the CCDS package
-from ccds.hook_utils.custom_config import write_custom_config
-from ccds.hook_utils.dependencies import write_dependencies
-
-#
-#  TEMPLATIZED VARIABLES FILLED IN BY COOKIECUTTER
-#
-packages = [
-    "black",
-    "flake8",
-    "isort",
-    "pip",
-    "python-dotenv",
-]
-if re.match("basic", "{{ cookiecutter.pydata_packages }}", re.I):
-    packages += [
-        "ipython",
-        "jupyter",
-        "matplotlib",
-        "numpy",
-        "pandas",
-        "scikit-learn",
-    ]
-# track packages that are not available through conda
-pip_only_packages = [
-    "awscli",
-    "python-dotenv",
-    "ipykernel",
-]
-
 ### Configure Git
 if re.match(r"y(es)?|o(ui)?", "{{ cookiecutter.first_time_git }}" , re.I):
     run(['git', 'config', '--global', '--add', 'safe.directory', '*'])
@@ -52,37 +21,21 @@ if re.match(r"y(es)?|o(ui)?", "{{ cookiecutter.init_git }}" , re.I):
     run(['git', 'remote', 'add', 'origin', f"git@github.com:alec42/{repo_name}.git"])
     run(['git', 'add', '.'])
     run(['git', 'commit', '-m', '\"Initial Commit\"'])
-    run(['gh', 'repo', 'create', repo_name])
+    if re.match(r"y(es)?|o(ui)?", "{{ cookiecutter.private_git_repo }}" , re.I):
+        run(['gh', 'repo', 'create', repo_name, '--private'])
+    elif re.match(r"no?n?", "{{ cookiecutter.private_git_repo }}" , re.I):
+        run(['gh', 'repo', 'create', repo_name, '--public'])
     run(['git', 'push', '--set-upstream', 'origin', 'main'])
     run(['git', 'switch', '-c', 'dev'])
     run(['git', 'push', '--set-upstream', 'origin', 'dev'])
 
-#
-#  POST-GENERATION FUNCTIONS
-#
-write_dependencies(
-    "{{ cookiecutter.dependency_file }}",
-    packages,
-    pip_only_packages,
-    repo_name=repo_name,
-    module_name="src",
-    python_version="{{ cookiecutter.python_version_number }}",
-)
 
 ### Run Makefile
 run(['make', 'create_environment'])
 
 
-# write_custom_config("{{ cookiecutter.custom_config }}")
-
 # Remove LICENSE if "No license file"
 if "{{ cookiecutter.open_source_license }}" == "No license file":
     Path("LICENSE").unlink()
-
-# Make single quotes prettier
-# Jinja tojson escapes single-quotes with \u0027 since it's meant for HTML/JS
-pyproject_text = Path("pyproject.toml").read_text()
-Path("pyproject.toml").write_text(pyproject_text.replace(r"\u0027", "'"))
-
 
 run(['make', 'clean'])
--- {{ cookiecutter.__repo_name }}/README.md
+++ {{ cookiecutter.__repo_name }}/README.md
@@ -14,30 +14,20 @@
 │   ├── processed      <- The final, canonical data sets for modeling.
 │   └── raw            <- The original, immutable data dump.
 │
-├── docs               <- A default Sphinx project; see sphinx-doc.org for details
-│
 ├── models             <- Trained and serialized models, model predictions, or model summaries
 │
 ├── notebooks          <- Jupyter notebooks. Naming convention is a number (for ordering),
 │                         the creator's initials, and a short `-` delimited description, e.g.
 │                         `1.0-jqp-initial-data-exploration`.
 │
-├── pyproject.toml     <- Project configuration file with package metadata for {{ cookiecutter.module_name }}
-│                         and configuration for tools like black
-│
-├── references         <- Data dictionaries, manuals, and all other explanatory materials.
-│
-├── reports            <- Generated analysis as HTML, PDF, LaTeX, etc.
-│   └── figures        <- Generated graphics and figures to be used in reporting
-│
-├── requirements.txt   <- The requirements file for reproducing the analysis environment, e.g.
-│                         generated with `pip freeze > requirements.txt`
+├── environment.yml    <- The enfironment file for reproducing the analysis environment, e.g.
+│                         generated with `conda create -f environment.yml`
 │
 ├── setup.cfg          <- Configuration file for flake8
 │
-└── {{ cookiecutter.module_name }}                <- Source code for use in this project.
+└── src                <- Source code for use in this project.
     │
-    ├── __init__.py    <- Makes {{ cookiecutter.module_name }} a Python module
+    ├── __init__.py    <- Makes src a Python module
     │
     ├── data           <- Scripts to download or generate data
     │   └── make_dataset.py
